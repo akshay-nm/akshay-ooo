8c2ef574df4bb2ce1b9e41bf59579d99a3019169
Akshay Kumar <akshay.nm92@gmail.com>
2021-01-06 07:58:49 +0530

    Add workaround related to postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,11 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
-    require('postcss-preset-env')({ stage: 1 }),
+    require('postcss-preset-env')({
+      stage: 1,
+      features: {
+        'focus-within-pseudo-class': false,
+      },
+    }),
   ],
 }

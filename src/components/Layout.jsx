b96a8d2ac6574afeb43090c5dd90e9b25dd72d97
Akshay Kumar <akshay.nm92@gmail.com>
2022-11-30 12:21:11 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -150,7 +150,7 @@ export function Layout({ children }) {
           <div className="mt-10 text-center lg:mt-12 lg:text-left">
             <p className="text-xl font-bold text-slate-900">
               <Link href="/">
-                <a>My scribe</a>
+                <a>What's this?</a>
               </Link>
             </p>
             <p className="mt-3 text-lg font-medium leading-8 text-slate-700">
